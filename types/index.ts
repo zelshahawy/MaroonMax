b196cb752b13950fbe7d68d9e93fbf3e13d0dbd1
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2026-04-05 19:53:33 -0500

    Added deps and types

---
+++ types/index.ts
@@ -0,0 +1,34 @@
+
+export type MovementType = "squat" | "bench" | "deadlift";
+export type LiftStatus = "waiting" | "lifitng" | "Success" | "fail";
+
+export interface Attempt {
+  weight: number | null;
+  status: LiftStatus;
+}
+
+export interface MeetStatus {
+  id: 1;
+  current_movement: MovementType;
+  current_round: 1 | 2 | 3;
+  is_round_active: boolean;
+  is_meet_active: boolean;
+}
+
+export interface Lifter {
+  id: string;
+  name: string;
+  squat_1: Attempt;
+  squat_2: Attempt;
+  squat_3: Attempt;
+  bench_1: Attempt;
+  bench_2: Attempt;
+  bench_3: Attempt;
+  deadlift_1: Attempt;
+  deadlift_2: Attempt;
+  deadlift_3: Attempt;
+  best_squat: number;
+  best_bench: number;
+  best_deadlift: number;
+  total: number;
+}
